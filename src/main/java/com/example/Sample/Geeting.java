662022f38c1ef00ee04785e2c07ddedeae16387b
souvik12 <ghosh.souvik03@gmail.com>
2020-02-17 22:48:20 +0530

    changes for docker

---
+++ src/main/java/com/example/Sample/Geeting.java
@@ -0,0 +1,15 @@
+package com.example.Sample;
+
+public class Geeting {
+	private String geeting;
+
+	public String getGeeting() {
+		return geeting;
+	}
+
+	public void setGeeting(String geeting) {
+		this.geeting = geeting;
+	}
+	
+
+}
--- src/main/java/com/example/Sample/SampleApplication.java
+++ src/main/java/com/example/Sample/SampleApplication.java
@@ -2,12 +2,22 @@ package com.example.Sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
 	}
-
+ @RequestMapping(value="/geeting")
+	 public Geeting getGeeting() {
+	 
+	 Geeting g=new Geeting();
+	 g.setGeeting("HI ALLL INDIA");
+	return g;
+	 
+ }
 }
